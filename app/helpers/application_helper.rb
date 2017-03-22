822d6de5a78152571d25b735f15a1e6ac102f09c
minhthuan274 <doanminhthuan274@gmail.com>
2017-03-22 10:48:35 +0700

    Static Pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def full_title(page_title='Capstone app')
+    page_title
+  end
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get root_path
+    assert_response :success
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+  end
+
+end
